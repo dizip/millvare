6652bc8983bb221fce72083d1f3c9c3d36ad6e6a
Mariusz Rozanski <contact.mariuszrozanski@gmail.com>
2020-12-29 14:59:39 +0100

    Initial dependency setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,31 @@
 module.exports = {
   siteMetadata: {
-    title: "millvare",
+    title: "Millvare",
+    author: "Mariusz Rozanski",
   },
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-layout`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Millvare`,
+        start_url: `/`,
+        background_color: `#02254f`,
+        theme_color: `#02254f`,
+        display: `minimal-ui`,
+        icon: `${__dirname}/src/assets/images/icon.png`,
+      },
+    },
+  ],
 };
